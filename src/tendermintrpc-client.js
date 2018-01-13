9b99eaf8cde3df7937d227b6be59f29568da7630
Zama Bandeira Braga <zama.braga@live.com>
2018-01-13 20:10:43 -0200

    add  status request

--- src/tendermintrpc-client.js
+++ src/tendermintrpc-client.js
@@ -1,3 +1,34 @@
 const { EventEmitter } = require('events')
+const axios = require('axios')
 
-module.exports = class TenderMintRPCClient extends EventEmitter {}
+/**
+ * make a http request
+ * @param {*} url address to request
+ * @param {*} params data send in request
+ */
+const makeRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: url,
+    data: {
+      method: 'status',
+      jsonrpc: '2.0',
+      params: params || {},
+      id: 'dontcare'
+    }
+  })
+}
+module.exports = class TenderMintRPCClient extends EventEmitter {
+  /**
+   * Creates an instance of TenderMintRPCClient.
+   * @param {any} {url} tendermint rpc server
+   */
+  constructor({ url } = {}) {
+    super()
+    this._url = url || 'http://localhost:46657'
+  }
+
+  status() {
+    return makeRequest(this._url)
+  }
+}
--- test/client_jsonrpc_test.js
+++ test/client_jsonrpc_test.js
@@ -1,53 +1,19 @@
 const assert = require('assert')
-const axios = require('axios')
-
-const makeRequest = params =>
-  axios({
-    method: 'post',
-    url: 'http://localhost:46657',
-    data: {
-      method: 'status',
-      jsonrpc: '2.0',
-      params: params || {},
-      id: 'dontcare'
-    }
-  })
+const TendermintRPCClient = require('./../src/tendermintrpc-client')
 
 describe('JSONRPC', () => {
-  const tests = [
-    { method: 'abci_info' },
-    { method: 'dump_consensus_state' },
-    { method: 'genesis' },
-    { method: 'net_info' },
-    { method: 'num_unconfirmed_txs' },
-    { method: 'status' },
-    { method: 'unconfirmed_txs' },
-    { method: 'unsafe_flush_mempool' },
-    { method: 'unsafe_stop_cpu_profiler' },
-    { method: 'validators' },
-    { method: 'abci_query' },
-    { method: 'block' },
-    { method: 'blockchain' },
-    { method: 'broadcast_tx_async' },
-    { method: 'broadcast_tx_commit' },
-    { method: 'broadcast_tx_sync' },
-    { method: 'commit' },
-    { method: 'dial_seeds' },
-    { method: 'subscribe' },
-    { method: 'tx' },
-    { method: 'unsafe_start_cpu_profiler' },
-    { method: 'unsafe_write_heap_profile' },
-    { method: 'unsubscribe' }
-  ]
+  const client = new TendermintRPCClient()
 
   describe('#over HTTP', () => {
-    tests.forEach(t => {
-      it(`shoud request method ${t.method}`, async () => {
-        const { data } = await makeRequest('teste')
-        const { jsonrpc, id } = data
-        assert.equal(jsonrpc, '2.0')
-        assert.equal(id, 'dontcare')
+    Object.getOwnPropertyNames(TendermintRPCClient.prototype)
+      .filter((e) => e !== 'constructor')
+      .forEach((t) => {
+        it(`shoud request method ${t}`, async () => {
+          const { data } = await client[t]()
+          const { jsonrpc, id } = data
+          assert.equal(jsonrpc, '2.0')
+          assert.equal(id, 'dontcare')
+        })
       })
-    })
   })
 })
